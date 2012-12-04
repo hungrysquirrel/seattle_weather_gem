4528d7249c8b5ddc2880b262662fcd782e59e248
Josh Rubinstein <josh@estately.com>
2012-12-03 22:10:00 -0800

    using the keys method on the hash retured from the api I see that there is a key called list that has the data for temperature I want

--- lib/seattle_weather_gem.rb
+++ lib/seattle_weather_gem.rb
@@ -4,5 +4,8 @@ require 'json'
 class SeattleWeather 
   def self.current_temperature
     data = JSON.parse(open("http://openweathermap.org/data/2.1/forecast/city?q=Seattle").read)
+
+    temperature_data = data['list'].first
+    the_temp = temperature_data['main'].fetch("temp")
   end
 end
