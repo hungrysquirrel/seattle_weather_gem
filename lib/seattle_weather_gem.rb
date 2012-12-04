7ea038eb435a22011581089ddf74c94de8111d36
Josh Rubinstein <josh@estately.com>
2012-12-03 22:07:24 -0800

    write a class method for current_temperature and use modules to pull in Seattle weather data from OpenWeatherMap

--- lib/seattle_weather_gem.rb
+++ lib/seattle_weather_gem.rb
@@ -2,4 +2,7 @@ require 'open-uri'
 require 'json'
 
 class SeattleWeather 
+  def self.current_temperature
+    data = JSON.parse(open("http://openweathermap.org/data/2.1/forecast/city?q=Seattle").read)
+  end
 end
