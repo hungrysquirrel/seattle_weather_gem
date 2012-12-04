e6982cd0a00c96ddb0dee1906ebab05665b076ad
Josh Rubinstein <josh@estately.com>
2012-12-03 22:20:29 -0800

    convert Kelvin to Fahrenheit

--- lib/seattle_weather_gem.rb
+++ lib/seattle_weather_gem.rb
@@ -6,8 +6,9 @@ class SeattleWeather
     data = JSON.parse(open("http://openweathermap.org/data/2.1/forecast/city?q=Seattle").read)
 
     temperature_data = data['list'].first
-    the_temp = temperature_data['main'].fetch("temp")
+    the_temp_kelvin = temperature_data['main'].fetch("temp")
+    the_temp_fahrenheit = (((the_temp_kelvin - 273) * 1.8 ) + 32).round
 
-    puts "Seattle's current temperature is #{the_temp}"
+    puts "Seattle's current temperature is #{the_temp_fahrenheit} degrees Fahrenheit"
   end
 end
