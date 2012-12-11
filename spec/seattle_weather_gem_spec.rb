074e0e2724881276de3d43847f9dedbc78c53c94
Josh Rubinstein <josh@estately.com>
2012-12-10 22:29:49 -0800

    stub out my data_feed method and test the returned string of the current_temp method

--- spec/seattle_weather_gem_spec.rb
+++ spec/seattle_weather_gem_spec.rb
@@ -1,7 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+require_relative '../lib/seattle_weather_gem.rb'
+
 describe "SeattleWeatherGem" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "prints #current_temp" do
+    seattle_temp = SeattleWeather.new
+    seattle_temp.stub(:data_feed).and_return('main' => {'temp' => 280})
+    seattle_temp.current_temp.should == "Seattle's current temperature is 45 degrees Fahrenheit"
   end
 end
